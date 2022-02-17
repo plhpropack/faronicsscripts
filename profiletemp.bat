9e86519554fc1d91f6a22fe1b7031dbee5b78f0f
plhpropack <97186721+plhpropack@users.noreply.github.com>
2022-02-17 15:41:22 +0000

    Add files via upload

---
+++ profiletemp.bat
@@ -0,0 +1,12 @@
+@(SETLOCAL
+  ECHO OFF
+  SET "_Path=C:\Users\%username%\AppData\Local\Temp\"
+)
+
+REM Delete all Subdirectories and their File Contents
+FOR /F "delims=:" %%_ IN ('
+  dir /B /A:D "%_Path%*" ') do (
+  RD /S /Q "%_Path%%%_\")
+
+REM Delete all files in Root Folder:
+DEL /F /Q  "%_Path%*" & DEL /F /Q /A:H "%_Path%*"
