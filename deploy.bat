1873447fbb7ce954f3c416b873935c0e5bf13601
plhpropack <97186721+plhpropack@users.noreply.github.com>
2022-02-21 15:27:47 +0000

    Add files via upload

---
+++ deploy.bat
@@ -0,0 +1,4 @@
+
+reg query HKEY_CLASSES_ROOT\{359C24F1-51B5-44CE-8F2D-2FBB1A0FE4EA}\FWA_GUI_Agent | Find "Name" > c:\plh\pc.txt
+reg query HKEY_CLASSES_ROOT\{359C24F1-51B5-44CE-8F2D-2FBB1A0FE4EA}\FWA_GUI_Agent > c:\plh\deploy.txt
+ for /f "tokens=3* delims= " %x in (c:\plh\pc.txt) do type c:\plh\deploy.txt > c:\plh\%x.txt
