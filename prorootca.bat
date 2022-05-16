1d8566556405dab0bb0dbd8d8608ce963b216df1
plhpropack <97186721+plhpropack@users.noreply.github.com>
2022-05-16 15:47:47 +0100

    Update prorootca.bat

--- prorootca.bat
+++ prorootca.bat
@@ -1,3 +1,3 @@
 copy \\w2k16adfs\wu$\Certificates\PropackRootCA.crt c:\PLH\PropackRootCA.crt
 Import-Certificate -FilePath "C:\plh\ProopackRootCA.crt" -CertStoreLocation Cert:\LocalMachine\Root
-del "c:\PLH\PropackRootCA.crt"
+REM del "c:\PLH\PropackRootCA.crt"
