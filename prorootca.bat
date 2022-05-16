bd4010394d2677c3123bc9a64404d5db27a4e930
plhpropack <97186721+plhpropack@users.noreply.github.com>
2022-05-16 15:55:44 +0100

    Update prorootca.bat

--- prorootca.bat
+++ prorootca.bat
@@ -1,6 +1,6 @@
 copy \\w2k16adfs\wu$\Certificates\PropackRootCA.crt c:\PLH\PropackRootCA.crt
 copy \\w2k16adfs\wu$\Certificates\PropackRootCA.ps1 c:\PLH\PropackRootCA.ps1
 powershell -ExecutionPolicy UnRestricted -file C:\plh\PropackRootCA.ps1
-REM Import-Certificate -FilePath "C:\plh\ProopackRootCA.crt" -CertStoreLocation Cert:\LocalMachine\Root
+REM Import-Certificate -FilePath "C:\plh\PropackRootCA.crt" -CertStoreLocation Cert:\LocalMachine\Root
 REM del "c:\PLH\PropackRootCA.crt"
 REM del "c:\PLH\PropackRootCA.ps1"
