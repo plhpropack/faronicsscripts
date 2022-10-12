67fee7db13685a74539a76d4def196be0d57c413
plhpropack <97186721+plhpropack@users.noreply.github.com>
2022-10-12 17:47:08 +0100

    Create simplefileshare.bat

---
+++ simplefileshare.bat
@@ -0,0 +1,2 @@
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\Folder\SharingWizardOn" /T REG_DWORD /V CheckedValue /D 0 /F
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\Folder\SharingWizardOn" /T REG_DWORD /V DefaultValue /D 0 /F
