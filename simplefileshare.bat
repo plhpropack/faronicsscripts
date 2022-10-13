a93fac9cfbbe307827d48d8d41deecb89d5ba0a9
plhpropack <97186721+plhpropack@users.noreply.github.com>
2022-10-13 09:23:06 +0100

    Update simplefileshare.bat

--- simplefileshare.bat
+++ simplefileshare.bat
@@ -1,2 +1,3 @@
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\Folder\SharingWizardOn" /T REG_DWORD /V CheckedValue /D 0 /F
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\Folder\SharingWizardOn" /T REG_DWORD /V DefaultValue /D 0 /F
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /T REG_DWORD /V SharingWizardOn /D 0 /F
