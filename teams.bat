4bc56fb3a0b578926de0afde1eba36ed20229d18
plhpropack <97186721+plhpropack@users.noreply.github.com>
2023-05-05 17:28:09 +0100

    Update teams.bat

--- teams.bat
+++ teams.bat
@@ -1,3 +1,3 @@
-copy \\w2k16adfs\wu$\Microsoft365\Teams_windows_x64.exe c:\plh\Teams_windows_x64.exe /Y
+copy \\w2k16adfs\wu$\Microsoft365\Teams_windows_x64.exe c:\PLH\Teams_windows_x64.exe /Y
 c:\plh\Teams_windows_x64.exe -s
-type teams > c:\plh\teams.txt
+echo teams > c:\plh\teams.txt
