1d835e202665e73d1d5e67aa9f93eb72f59dcdf8
plhpropack <97186721+plhpropack@users.noreply.github.com>
2023-05-05 17:46:20 +0100

    Update teams.bat

--- teams.bat
+++ teams.bat
@@ -1,5 +1,5 @@
 copy \\w2k16adfs\wu$\Microsoft365\Teams_windows_x64.exe c:\PLH\Teams_windows_x64.exe /Y
 REM copy \\w2k16adfs\wu$\PLH\Fonts\*.*tf c:\PLH\fonts /Y
 echo teams > c:\PLH\teams1.txt
-c:\plh\Teams_windows_x64.exe -s
+call c:\plh\Teams_windows_x64.exe -s
 echo teams > c:\PLH\teams2.txt
