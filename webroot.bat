4e23cb461ffc45da247fc54d24abf312d1fd9f97
plhpropack <97186721+plhpropack@users.noreply.github.com>
2025-01-03 12:49:20 +0000

    Add files via upload

--- webroot.bat
+++ webroot.bat
@@ -2,8 +2,18 @@ REM powershell -Command "Invoke-WebRequest https://downbox.webrootanywhere.com/w
 
 powershell -Command "Invoke-WebRequest https://anywhere.webrootcloudav.com/zerol/wsasme.msi -OutFile C:\PLH\wsasme.msi"
 
+powershell -Command Start-Process 'msiexec.exe' -ArgumentList '/I "C:\PLH\wsasme.msi" GUILIC=1954-cntm-cc35-9246-4d66 /qn /l*v c:\plh\install.log' -Wait
+
 REM https://anywhere.webrootcloudav.com/zerol/wsasme.msi
 
-Ping 127.0.0.1 -n 30
+REM Ping 127.0.0.1 -n 30
 
-call msiexec /i c:\plh\wsasme.msi GUILIC=1954-cntm-cc35-9246-4d66 CMDLINE=SME,quiet /qn /l*v c:\plh\install.log
+REM call msiexec /i c:\plh\wsasme.msi GUILIC=1954-cntm-cc35-9246-4d66 CMDLINE=SME,quiet /qn /l*v c:\plh\install.log
+
+
+REM (Start-Process "msiexec.exe" -ArgumentList "/i $dirFiles\ABDS2017\Img\x64\RVT\RVT.msi INSTALLDIR=""C:\Program Files\Autodesk\"" ADSK_SETUP_EXE=1 /qb!" -NoNewWindow -Wait -PassThru).ExitCode
+
+
+
+
+powershell -Command Start-Process 'msiexec.exe' -ArgumentList '/I "C:\PLH\wsasme.msi" GUILIC=1954-cntm-cc35-9246-4d66 /qn /l*v c:\plh\install.log' -Wait
