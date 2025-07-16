0ae337041d774c9db3e04975f5fc8c7fd9c19f39
plhpropack <97186721+plhpropack@users.noreply.github.com>
2025-07-16 10:57:02 +0100

    Update lock.bat

--- lock.bat
+++ lock.bat
@@ -6,8 +6,13 @@ powershell -c "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/plhprop
 xcopy "\\w2k16adfs\Propack Unclassifed (UN)\Propack\desktop.jpg" "C:\plh\desktop.jpg" /h /y
 xcopy "\\w2k16adfs\Propack Unclassifed (UN)\Propack\lock.jpg" "C:\plh\lock.jpg" /h /y
 
-
 REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP
 REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP /v LockScreenImagePath /t REG_SZ /d "C:\plh\lock.jpg" /f
 REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP /v LockScreenImageUrl /t REG_SZ /d "C:\plh\lock.jpg" /f
 REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP /v LockScreenImageStatus /t REG_DWORD /d 1 /f
+
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager /v "RotatingLockScreenEnabled" /t REG_DWORD  /d "0" /f
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager /v "RotatingLockScreenOverlayEnabled" /t REG_DWORD  /d "0" /f
+
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v "Wallpaper" /t REG_SZ /d "C:\plh\desktop.jpg" /f
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v "WallpaperStyle" /t REG_SZ  /d "4" /f
