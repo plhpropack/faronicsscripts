3aa10968aa02181f937c0882691e538c28020059
plhpropack <97186721+plhpropack@users.noreply.github.com>
2025-07-16 09:54:44 +0100

    Update Lock2.bat

--- Lock2.bat
+++ Lock2.bat
@@ -3,12 +3,12 @@
 
 
 
-reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP /v "LockScreenImagePath" /t REG_SZ /d "C:\\plh\\lock.jpg" /f > c:\plh\lock2.txt
-reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP /v "LockScreenImageUrl" /t REG_SZ /d "C:\\plh\\lock.jpg" /f >> c:\plh\lock2.txt
-reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP /v "LockScreenImageStatus" /t REG_DWORD  /d "1" /f >> c:\plh\lock2.txt
+reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP /v "LockScreenImagePath" /t REG_SZ /d "C:\\plh\\lock.jpg" /f > "c:\\plh\\lock2.txt"
+reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP /v "LockScreenImageUrl" /t REG_SZ /d "C:\\plh\\lock.jpg" /f >> "c:\\plh\\lock2.txt"
+reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP /v "LockScreenImageStatus" /t REG_DWORD  /d "1" /f >> "c:\\plh\\lock2.txt"
 
-reg add HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager /v "RotatingLockScreenEnabled" /t REG_DWORD  /d "0" /f >> c:\plh\lock2.txt
-reg add HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager /v "RotatingLockScreenOverlayEnabled" /t REG_DWORD  /d "0" /f >> c:\plh\lock2.txt
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager /v "RotatingLockScreenEnabled" /t REG_DWORD  /d "0" /f >> "c:\\plh\\lock2.txt"
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager /v "RotatingLockScreenOverlayEnabled" /t REG_DWORD  /d "0" /f >> "c:\\plh\\lock2.txt"
 
-reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v "Wallpaper" /t REG_SZ /d "C:\\plh\\desktop.jpg" /f >> c:\plh\lock2.txt
-reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v "Wallpaper" /t REG_SZ  /d "C:\\plh\\desktop.jpg" /f >> c:\plh\lock2.txt
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v "Wallpaper" /t REG_SZ /d "C:\\plh\\desktop.jpg" /f >> "c:\\plh\\lock2.txt"
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v "Wallpaper" /t REG_SZ  /d "C:\\plh\\desktop.jpg" /f >> "c:\\plh\\lock2.txt"
