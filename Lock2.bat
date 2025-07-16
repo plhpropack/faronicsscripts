e675470f593aafb2bc29aec9940a2c5981a362b9
plhpropack <97186721+plhpropack@users.noreply.github.com>
2025-07-16 10:09:23 +0100

    Update Lock2.bat

--- Lock2.bat
+++ Lock2.bat
@@ -3,10 +3,10 @@ md c:\PLH
 
 Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/plhpropack/faronicsscripts/main/lock.jpg' -OutFile 'c:\plh\lock.jpg'
 Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/plhpropack/faronicsscripts/main/desktop.jpg' -OutFile 'c:\plh\desktop.jpg'
-#Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/plhpropack/faronicsscripts/refs/heads/main/lockscreen.reg'  -OutFile 'c:\plh\lockscreen.reg'
+REM Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/plhpropack/faronicsscripts/refs/heads/main/lockscreen.reg'  -OutFile 'c:\plh\lockscreen.reg'
 
-#xcopy "\\w2k16adfs\Propack Unclassifed (UN)\Propack\desktop.jpg" "C:\plh\desktop.jpg" /h /y
-#xcopy "\\w2k16adfs\Propack Unclassifed (UN)\Propack\lock.jpg" "C:\plh\lock.jpg" /h /y
+REM xcopy "\\w2k16adfs\Propack Unclassifed (UN)\Propack\desktop.jpg" "C:\plh\desktop.jpg" /h /y
+REM xcopy "\\w2k16adfs\Propack Unclassifed (UN)\Propack\lock.jpg" "C:\plh\lock.jpg" /h /y
 
 reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP /v "LockScreenImagePath" /t REG_SZ /d "C:\plh\lock.jpg" /f
 reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP /v "LockScreenImageUrl" /t REG_SZ /d "C:\plh\lock.jpg" /f
